7a48624c1c05e7095c301a8411b6fefdc231a729
Yeray <thewalkerofdreams@gmail.com>
2020-01-02 13:37:06 +0100

    commit

--- Gallinas/app/src/main/java/es/iesnervion/yeray/gallinas/Fragments/EditHenFragment.java
+++ Gallinas/app/src/main/java/es/iesnervion/yeray/gallinas/Fragments/EditHenFragment.java
@@ -1,31 +1,103 @@
 package es.iesnervion.yeray.gallinas.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import es.iesnervion.yeray.gallinas.DDBB.ManejadorBaseDeDatos;
+import es.iesnervion.yeray.gallinas.Entities.Gallina;
+import es.iesnervion.yeray.gallinas.HenListActivity;
+import es.iesnervion.yeray.gallinas.MainActivity;
 import es.iesnervion.yeray.gallinas.R;
 import es.iesnervion.yeray.gallinas.ViewModels.HenListActivityVM;
 
 public class EditHenFragment extends Fragment {
 
+    EditText name, pedigree, weight, height;
     HenListActivityVM henListActivityVM;
+    Button btnUpdate;
     //Constructor por defecto.
     public EditHenFragment(){
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+        View view = inflater.inflate(R.layout.activity_edit_hen_fragment, container, false);
         henListActivityVM = ViewModelProviders.of(getActivity()).get(HenListActivityVM.class);
 
-        View view = inflater.inflate(R.layout.activity_edit_hen_fragment, container, false);
+        name = view.findViewById(R.id.EditTextNombreGallina);
+        pedigree = view.findViewById(R.id.EditTextPedigriGallina);
+        weight = view.findViewById(R.id.EditTextPesoGallina);
+        height = view.findViewById(R.id.EditTextAlturaGallina);
+        btnUpdate = view.findViewById(R.id.btnModHen);
+
+        if(henListActivityVM.getGallinaSeleccionada().getValue() != null){
+            name.setText(henListActivityVM.getGallinaSeleccionada().getValue().getNombre());
+            pedigree.setText(henListActivityVM.getGallinaSeleccionada().getValue().getPedigri());
+            weight.setText(String.valueOf(henListActivityVM.getGallinaSeleccionada().getValue().getPeso()));
+            height.setText(String.valueOf(henListActivityVM.getGallinaSeleccionada().getValue().getAltura()));
+        }
+
+        //Los observers
+        final Observer<Gallina> contactObserver = new Observer<Gallina>() {
+            @Override
+            public void onChanged(Gallina gallina) {
+                if(henListActivityVM.getGallinaSeleccionada().getValue() != null){
+                    name.setText(henListActivityVM.getGallinaSeleccionada().getValue().getNombre());
+                    pedigree.setText(henListActivityVM.getGallinaSeleccionada().getValue().getPedigri());
+                    weight.setText(String.valueOf(henListActivityVM.getGallinaSeleccionada().getValue().getPeso()));
+                    height.setText(String.valueOf(henListActivityVM.getGallinaSeleccionada().getValue().getAltura()));
+                }
+            }
+        };
+
+        //Observamos los LiveData
+        henListActivityVM.getGallinaSeleccionada().observe(this, contactObserver);
+
+        btnUpdate.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if(!name.getText().toString().equals("")){
+                    if(!pedigree.getText().toString().equals("")){
+                        if(!weight.getText().toString().equals("") && Double.valueOf(weight.getText().toString()) > 0 &&
+                                Double.valueOf(weight.getText().toString()) < 30){
+                            if(!height.getText().toString().equals("") && Double.valueOf(height.getText().toString()) > 0 &&
+                                    Double.valueOf(height.getText().toString()) < 3){
+                                henListActivityVM.getGallinaSeleccionada().getValue().setNombre(name.getText().toString());//Modificamos la gallina seleccionada
+                                henListActivityVM.getGallinaSeleccionada().getValue().setPedigri(pedigree.getText().toString());
+                                henListActivityVM.getGallinaSeleccionada().getValue().setPeso(Double.valueOf(weight.getText().toString()));
+                                henListActivityVM.getGallinaSeleccionada().getValue().setAltura(Double.valueOf(height.getText().toString()));
+
+                                ManejadorBaseDeDatos manejador = new ManejadorBaseDeDatos(getContext());
+                                manejador.updateHen(henListActivityVM.getGallinaSeleccionada().getValue());
+                                ((HenListActivity) getActivity()).recargarLista();//Recargamos la lista del mainActivity.
+                                Toast.makeText(getContext(), "Se ha modificado la gallina.", Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(getContext(), "La altura debe ser entre 0 y 3.", Toast.LENGTH_SHORT).show();
+                            }
+                        }else{
+                            Toast.makeText(getContext(), "El peso debe ser entre 0 y 30.", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(getContext(), "El pedigrí se sencuentra vacío.", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(getContext(), "El nombre se sencuentra vacío.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         return view;
     }
--- Gallinas/app/src/main/java/es/iesnervion/yeray/gallinas/HenListActivity.java
+++ Gallinas/app/src/main/java/es/iesnervion/yeray/gallinas/HenListActivity.java
@@ -1,27 +1,35 @@
 package es.iesnervion.yeray.gallinas;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import es.iesnervion.yeray.gallinas.DDBB.ManejadorBaseDeDatos;
 import es.iesnervion.yeray.gallinas.Entities.Gallina;
+import es.iesnervion.yeray.gallinas.Fragments.EditHenFragment;
 import es.iesnervion.yeray.gallinas.Lists.AdapterHenList;
-import es.iesnervion.yeray.gallinas.ViewModels.CreateCountActivityVM;
 import es.iesnervion.yeray.gallinas.ViewModels.HenListActivityVM;
 
-public class HenListActivity extends AppCompatActivity {
+public class HenListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
     ListView listView;
     ArrayList<Gallina> listadoGallinas = new ArrayList<>();
     AdapterHenList adapter;
     HenListActivityVM henListActivityVM;
+    EditHenFragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,8 +44,80 @@ public class HenListActivity extends AppCompatActivity {
         adapter = new AdapterHenList(this, R.layout.item_list, listadoGallinas);
         listView.setAdapter(adapter);
 
-        //listView.setOnItemClickListener(this);
-        //listView.setOnItemLongClickListener(this);
+        listView.setOnItemClickListener(this);
+        listView.setOnItemLongClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if(henListActivityVM.getGallinaSeleccionada() == null || fragment == null){//Si no hay ninguna gallina seleccionada o el fragmento aún no ha sido instanciado
+            replaceFragment();
+        }
+        henListActivityVM.setGallinaSeleccionada((Gallina) listView.getAdapter().getItem(position));//Obtenemos la gallina seleccionada
+    }
+
+    @Override
+    public boolean onItemLongClick (AdapterView<?> adapterView, View view,int i, long l){
+        final Gallina item = (Gallina) adapterView.getItemAtPosition(i);//Obtenemos el item de la posición clicada
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Confirm Delete");// Setting Alert Dialog Title
+        alertDialogBuilder.setMessage("Do you really want delete this hen?");// Setting Alert Dialog Message
+        alertDialogBuilder.setCancelable(false);//Para que no podamos quitar el dialogo sin contestarlo
+
+        alertDialogBuilder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                Toast.makeText(getBaseContext(), "Hen deleted", Toast.LENGTH_SHORT).show();
+                ManejadorBaseDeDatos manejador = new ManejadorBaseDeDatos(getApplication());
+                manejador.deleteHen(item.getId());
+                removeYourFragment();
+                recargarLista();
+            }
+        });
+
+        alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+        return true;//Nos permite no realizar la acción de clicado rápido cuando dejamos pulsado un item.
+    }
+
+    /*
+    * Interfaz
+    * Nombre: replaceFragment
+    * Comentario: Este método nos permite crear un fragmento y remplazar el contenido de nuestro
+    * FrameLayout por ese mismo fragmento.
+    * Cabecera: public void replaceFragment()
+    * Postcondiciones: El método reemplaza el contenido del FrameLayout por el nuevo fragmento.
+    * */
+    public void replaceFragment(){
+        fragment = new EditHenFragment();
+        FragmentTransaction transation = getSupportFragmentManager().beginTransaction();
+        transation.replace(R.id.FrameLayout01, fragment);
+        transation.commit();
+    }
+
+    /*
+    * Interfaz
+    * Nombre: removeYourFragment
+    * Comentario: Este método nos permite eliminar el fragmento de la actividad actual.
+    * Cabecera: public void removeYourFragment()
+    * Postcondiciones: El método elimina el fragmento de la actividad actual.
+    * */
+    public void removeYourFragment(){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        if (fragment != null) {
+            transaction.remove(fragment);
+            transaction.commit();
+            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE);
+            fragment = null;
+        }
     }
 
     /*
@@ -53,5 +133,23 @@ public class HenListActivity extends AppCompatActivity {
         Intent i = new Intent(this, CreateHenActivity.class);
         i.putExtra("nombreUsuario", henListActivityVM.getNickUsuario());
         startActivity(i);
+    }
+
+    /*
+     * Interfaz
+     * Nombre: recargarLista
+     * Comentario: Este método nos permite recargar la lista de gallinas.
+     * Cabecera: public void recargarLista()
+     * Postcondiciones: El método recarga la lista de gallinas.
+     * */
+    public void recargarLista(){
+        ManejadorBaseDeDatos manejador = new ManejadorBaseDeDatos(this);
+        ArrayList<Gallina> listaActualizada = new ArrayList<Gallina>();
+
+        listaActualizada.addAll(manejador.getAllHens(henListActivityVM.getNickUsuario()));//Cargamos las gallinas en el arraylist
+        henListActivityVM.setListadoGallinas(listaActualizada);//Actualizamos el vm
+        listadoGallinas = henListActivityVM.getListadoGallinas().getValue();
+        adapter = new AdapterHenList(this, R.layout.item_list, listadoGallinas);
+        listView.setAdapter(adapter);
     }
 }
